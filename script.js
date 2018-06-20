bca9930a3d60ef64558953ad18d12c831c6eb481
rahul <officialrahulmandal@gmail.com>
2018-06-20 15:39:09 +0530

    Get the current hour, minute, and second with the Date() object

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ let hrPosition = 20;
 let minPosition = 130;
 let secPosition = 267;
 
+var date = new Date()
+let hr =  date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log("hour "+ hr + " minutes "+ min + " seconds  "+ sec );
+
 
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
 MINUTEHAND.style.transform = "rotate("+ minPosition +"deg)";
